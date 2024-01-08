7dbefca221190d59159cb6993e440c3464701f2b
Cyndra-project <junaidgohan123@gmail.com>
2024-01-08 18:48:48 +0100

    chore: update axum examples to axum 0.7 (#123)

--- axum/static-next-server/src/main.rs
+++ axum/static-next-server/src/main.rs
@@ -1,12 +1,12 @@
-use std::path::PathBuf;
-
 use axum::Router;
-use axum_extra::routing::SpaRouter;
+use tower_http::services::{ServeDir, ServeFile};
 
 #[cyndra_runtime::main]
 async fn axum() -> cyndra_axum::CyndraAxum {
-    let router =
-        Router::new().merge(SpaRouter::new("/", PathBuf::from("static")).index_file("index.html"));
+    let router = Router::new().nest_service(
+        "/",
+        ServeDir::new("static").not_found_service(ServeFile::new("static/index.html")),
+    );
 
     Ok(router.into())
 }
