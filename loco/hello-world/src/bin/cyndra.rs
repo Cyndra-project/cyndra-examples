c7e794751094e6ea2450033f09f596f893473556
Cyndra-project <junaidgohan123@gmail.com>
2025-01-14 13:34:54 +0100

    chore: bump loco to 0.14.0 (#202)
    * chore: loco 0.14.0 + cyndra 0.51.0
    
    * fix: package name
    
    * update gitignore from cyndra cli

--- loco/hello-world/src/bin/cyndra.rs
+++ loco/hello-world/src/bin/cyndra.rs
@@ -1,16 +1,25 @@
-use hello_world::app::App;
 use loco_rs::boot::{create_app, StartMode};
 use loco_rs::environment::Environment;
+use hello_world::app::App;
+
+use cyndra_runtime::DeploymentMetadata;
 
 #[cyndra_runtime::main]
 async fn main(
-    #[cyndra_runtime::Metadata] meta: cyndra_runtime::DeploymentMetadata,
+  
+  #[cyndra_runtime::Metadata] meta: DeploymentMetadata,
 ) -> cyndra_axum::CyndraAxum {
+    
     let environment = match meta.env {
         cyndra_runtime::Environment::Local => Environment::Development,
         cyndra_runtime::Environment::Deployment => Environment::Production,
     };
-    let boot_result = create_app::<App>(StartMode::ServerOnly, &environment)
+
+     let config = environment
+        .load()
+        .expect("Failed to load configuration from the environment");
+    
+    let boot_result = create_app::<App>(StartMode::ServerOnly, &environment, config)
         .await
         .unwrap();
 
